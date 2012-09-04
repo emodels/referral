4c69ea8b8eca7f260ce3fb3a198db3e9d808ee42
danesh <danesh@danesh-lap.mshome.net>
2012-09-04 21:26:56 +0530

    AJAX Progress indicator added

--- protected/views/admin/entry/index.php
+++ protected/views/admin/entry/index.php
@@ -1,8 +1,3 @@
-<script type="text/javascript">
-    function LoadGrid(){
-        //$('#yt0').click();
-    }
-</script>
 <div style="float: left">
     <?php require_once Yii::getPathOfAlias('webroot.protected.views.admin') . '/admin_header_include.php'; ?>
 </div>  
@@ -12,16 +7,31 @@
         <div class="column" style="padding-top: 2px">Partner Company :</div>
         <div class="column"><?php echo CHtml::dropDownList('Entry[referrel_user]', '', CHtml::listData(User::model()->findAll('user_type = :user_type', array(':user_type'=>'1')),'id','company'), array('empty'=>'Select Partner',
                                        'ajax' => array('type'=>'POST','url'=>CController::createUrl('ListStatus'),
+                                       'beforeSend' => 'function(){
+                                            $("#divProgress").show();
+                                       }',
                                        'success'=>'js:function(data) {
-                                            LoadGrid();
+                                            $("#divProgress").hide();
                                             $("#Entry_status").html(data);
                                         }'))); ?></div>
         <div class="column" style="padding-top: 2px; padding-left: 40px">Status :</div>
         <div class="column"><?php echo CHtml::dropDownList('Entry[status]', '', array(), array('empty'=>'Select Status')); ?></div>
-        <div class="column" style="padding-left: 40px"><?php echo CHtml::ajaxButton('List','', array('type'=>'POST','update'=>'#divGrid'), array('class'=>'button','style'=>'padding:2px 10px 2px 10px')) ?></div>    
+        <div class="column" style="padding-left: 40px"><?php echo CHtml::ajaxButton('List','', array('type'=>'POST',
+                                       'beforeSend' => 'function(){
+                                            $("#divProgress").show();
+                                       }',
+                                       'success'=>'js:function(data) {
+                                            $("#divProgress").hide();
+                                            $("#divGrid").html(data);
+                                       }'), array('class'=>'button','style'=>'padding:2px 10px 2px 10px')); ?>
+        </div>
     </div>
     <div class="row"><hr style="padding-top: 2px"/></div>
     <?php echo CHtml::endForm();  ?>
+    <div id="divProgress" style="display: none; padding-bottom: 10px">
+        <span><img src="<?php echo Yii::app()->baseUrl; ?>/images/ajax-loader2.gif" style="width: 40px; vertical-align: middle"/></span>
+        <span style="font-size: 16px; color: green"><b>Loading results . . .</b></span>
+    </div>
     <div id="divGrid">
     <?php
     if ($ShowAll == TRUE) {
